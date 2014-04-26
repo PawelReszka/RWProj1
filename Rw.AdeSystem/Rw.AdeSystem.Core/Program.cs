e0f98996812562654b842b4c2579d0f96ccc86f0
adamczykm <michal.anks@gmail.com>
2014-04-27 00:48:42 +0200

    gui project added

--- Rw.AdeSystem/Rw.AdeSystem.Core/Program.cs
+++ Rw.AdeSystem/Rw.AdeSystem.Core/Program.cs
@@ -1,15 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using SbsSW.SwiPlCs;
 
 namespace Rw.AdeSystem.Core
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
+            //Environment.SetEnvironmentVariable("SWI_HOME_DIR", @"C:\swipl\bin");
+            if (PlEngine.IsInitialized) return;
+            String[] param = { "-q" };  // suppressing informational and banner messages
+            PlEngine.Initialize(param);
+            PlQuery.PlCall("assert(father(martin, inka))");
+            PlQuery.PlCall("assert(father(uwe, gloria))");
+            PlQuery.PlCall("assert(father(uwe, melanie))");
+            PlQuery.PlCall("assert(father(uwe, ayala))");
+            using (var q = new PlQuery("father(P, C), atomic_list_concat([P,' is_father_of ',C], L)"))
+            {
+                foreach (var v in q.SolutionVariables)
+                    Console.WriteLine(v["L"].ToString());
+
+                Console.WriteLine("all child's from uwe:");
+                q.Variables["P"].Unify("uwe");
+                foreach (var v in q.SolutionVariables)
+                    Console.WriteLine(v["C"].ToString());
+            }
+            PlEngine.PlCleanup();
+            Console.WriteLine("finshed!");
         }
     }
 }
