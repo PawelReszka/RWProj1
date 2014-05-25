00b8aac863e10dd8ad83651ffd0b04e9f77f24a6
Tomasz Półgrabia <tpsa.s.a@gmail.com>
2014-05-25 18:37:28 +0200

    Dodano weryfikujący test resów dla example2 - 2 nie działają.

--- Rw.AdeSystem/prolog/example2_test.pl
+++ Rw.AdeSystem/prolog/example2_test.pl
@@ -65,5 +65,101 @@ test(entice_mietus_0) :-
     resN_trunc(entice, mietus, state0, [state0]),
     resAb_trunc(entice, mietus, state0, []).
 
+test(entice_mietus_1) :-
+    res0(entice, mietus, state1, [state0, state1,state2,state3,state4,state5]),
+    res0_min(entice,mietus, state1, [state1]),
+    res0_plus(entice, mietus, state1, [state0, state1]),
+    resN(entice, mietus, state1, [state1]),
+    resAb(entice, mietus, state1, []),
+    resN_trunc(entice, mietus, state1, [state1]),
+    resAb_trunc(entice, mietus, state1, []).
+
+test(entice_mietus_2) :-
+    res0(entice, mietus, state2, [state0, state1,state2,state3,state4,state5]),
+    res0_min(entice,mietus, state2, [state2]),
+    res0_plus(entice, mietus, state2, [state0, state1]),
+    resN(entice, mietus, state2, [state0]),
+    resAb(entice, mietus, state2, [state2]),
+    resN_trunc(entice, mietus, state2, [state0]),
+    resAb_trunc(entice, mietus, state2, [state2]).
+
+test(entice_mietus_3) :-
+    res0(entice, mietus, state3, [state0, state1,state2,state3,state4,state5]),
+    res0_min(entice,mietus, state3, [state3]),
+    res0_plus(entice, mietus, state3, [state0, state1]),
+    resN(entice, mietus, state3, [state1]),
+    resAb(entice, mietus, state3, [state3]),
+    resN_trunc(entice, mietus, state3, [state1]),
+    resAb_trunc(entice, mietus, state3, [state3]).
+
+test(entice_mietus_4) :-
+    res0(entice, mietus, state4, [state0, state1,state2,state3,state4,state5]),
+    res0_min(entice,mietus, state4, [state4]),
+    res0_plus(entice, mietus, state4, [state0, state1]),
+    resN(entice, mietus, state4, [state0]),
+    resAb(entice, mietus, state4, [state4]),
+    resN_trunc(entice, mietus, state4, [state0]),
+    resAb_trunc(entice, mietus, state4, [state4]).
+
+test(entice_mietus_5) :-
+    res0(entice, mietus, state5, [state0, state1,state2,state3,state4,state5]),
+    res0_min(entice,mietus, state5, [state5]),
+    res0_plus(entice, mietus, state5, [state0, state1]),
+    resN(entice, mietus, state5, [state1]),
+    resAb(entice, mietus, state5, [state5]),
+    resN_trunc(entice, mietus, state5, [state1]),
+    resAb_trunc(entice, mietus, state5, [state5]).
+
+test(shoot_hador_0) :-
+    resN(shoot,hador,state0,[state4]),
+    resAb(shoot,hador,state0,[state0]).
+
+test(shoot_hador_1) :-
+    resN(shoot,hador,state1,[state1]),
+    resAb(shoot,hador,state1,[]).
+
+test(shoot_hador_2) :-
+    resN(shoot,hador,state2,[state4]),
+    resAb(shoot,hador,state2,[state0]).
+
+test(shoot_hador_3) :-
+    resN(shoot,hador,state3,[state3]),
+    resAb(shoot,hador,state3,[]).
+
+test(shoot_hador_4) :-
+    resN(shoot,hador,state4,[state4]),
+    resAb(shoot,hador,state4,[state0]).
+
+test(shoot_hador_5) :-
+    resN(shoot,hador,state5,[state5]),
+    resAb(shoot,hador,state5,[]).
+
+test(shoot_mietus_0) :-
+    resN(shoot,mietus,state0,[state0]),
+    resAb(shoot,mietus,state0,[]).
+
+test(shoot_mietus_1) :-
+    resN(shoot,mietus,state1,[state5]),
+    resAb(shoot,mietus,state1,[]).
+
+test(shoot_mietus_2) :-
+    resN(shoot,mietus,state2,[state2]),
+    resAb(shoot,mietus,state2,[]).
+
+test(shoot_mietus_3) :-
+    resN(shoot,mietus,state3,[state5]),
+    resAb(shoot,mietus,state3,[]).
+
+test(shoot_mietus_4) :-
+    resN(shoot,mietus,state4,[state4]),
+    resAb(shoot,mietus,state4,[]).
+
+test(shoot_mietus_5) :-
+    resN(shoot,mietus,state5,[state5]),
+    resAb(shoot,mietus,state5,[]).
+
+
+
+
 
 :- end_tests(resy_example2).
