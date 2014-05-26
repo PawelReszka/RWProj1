309f04a610600174b9537054cc4ff4044984d172
Tomasz Półgrabia <tpsa.s.a@gmail.com>
2014-05-26 14:14:25 +0200

    Dodano połowę testu do example3.

---
+++ Rw.AdeSystem/prolog/example3_test.pl
@@ -0,0 +1,77 @@
+:- [engine],[example3].
+
+:- begin_tests(resy_example3).
+
+test(turn1_hador_0) :-
+    res0(turn1, hador, state0, [state1, state2]),
+    res0_min(turn1, hador, state0, [state1]),
+    res0_plus(turn1, hador, state0, [state1,state2]),
+    resN(turn1,hador,state0,[state1]),
+    resAb(turn1, hador, state0, []),
+    resN_trunc(turn1,hador,state0,[state1]),
+    resAb_trunc(turn1, hador, state0, []).
+
+test(turn1_hador_1) :-
+    res0(turn1, hador, state1, [state0, state3]),
+    res0_min(turn1, hador, state1, [state0]),
+    res0_plus(turn1, hador, state1, [state0,state3]),
+    resN(turn1,hador,state1,[state0]),
+    resAb(turn1, hador, state1, []),
+    resN_trunc(turn1,hador,state1,[state0]),
+    resAb_trunc(turn1, hador, state1, []).
+
+test(turn1_hador_2) :-
+    res0(turn1, hador, state2, [state0, state3]),
+    res0_min(turn1, hador, state2, [state3]),
+    res0_plus(turn1, hador, state2, [state0,state3]),
+    resN(turn1,hador,state2,[state3]),
+    resAb(turn1, hador, state2, []),
+    resN_trunc(turn1,hador,state2,[state3]),
+    resAb_trunc(turn1, hador, state2, []).
+
+test(turn1_hador_3) :-
+    res0(turn1, hador, state3, [state1, state2]),
+    res0_min(turn1, hador, state3, [state2]),
+    res0_plus(turn1, hador, state3, [state1,state2]),
+    resN(turn1,hador,state3,[state2]),
+    resAb(turn1, hador, state3, []),
+    resN_trunc(turn1,hador,state3,[state2]),
+    resAb_trunc(turn1, hador, state3, []).
+
+test(turn1_mietus_0) :-
+    res0(turn1, mietus, state0, [state0,state1, state2,state3]),
+    res0_min(turn1, mietus, state0, [state0]),
+    res0_plus(turn1, mietus, state0, [state1,state2]),
+    resN(turn1,mietus,state0,[state1]),
+    resAb(turn1, mietus, state0, []),
+    resN_trunc(turn1,mietus,state0,[state1]),
+    resAb_trunc(turn1, mietus, state0, []).
+
+test(turn1_mietus_1) :-
+    res0(turn1, mietus, state1, [state0, state1,state2,state3]),
+    res0_min(turn1, mietus, state1, [state1]),
+    res0_plus(turn1, mietus, state1, [state0,state3]),
+    resN(turn1,mietus,state1,[state0]),
+    resAb(turn1, mietus, state1, [state1]),
+    resN_trunc(turn1,mietus,state1,[state0]),
+    resAb_trunc(turn1, mietus, state1, [state1]).
+
+test(turn1_mietus_2) :-
+    res0(turn1, mietus, state2, [state0, state1,state2,state3]),
+    res0_min(turn1, mietus, state2, [state2]),
+    res0_plus(turn1, mietus, state2, [state0,state3]),
+    resN(turn1,mietus,state2,[state3]),
+    resAb(turn1, mietus, state2, [state2]),
+    resN_trunc(turn1,mietus,state2,[state3]),
+    resAb_trunc(turn1, mietus, state2, [state2]).
+
+test(turn1_mietus_3) :-
+    res0(turn1, mietus, state3, [state0,state1, state2,state3]),
+    res0_min(turn1, mietus, state3, [state3]),
+    res0_plus(turn1, mietus, state3, [state1,state2]),
+    resN(turn1,mietus,state3,[state2]),
+    resAb(turn1, mietus, state3, [state3]),
+    resN_trunc(turn1,mietus,state3,[state2]),
+    resAb_trunc(turn1, mietus, state3, [state3]).
+
+:- end_tests(resy_example3).
