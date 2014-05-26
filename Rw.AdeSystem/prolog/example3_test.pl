b360da2a16ab9ff0e2402a3a453e8459be95411d
Tomasz Półgrabia <tpsa.s.a@gmail.com>
2014-05-26 21:05:46 +0200

    Kolejne poprawki.

--- Rw.AdeSystem/prolog/example3_test.pl
+++ Rw.AdeSystem/prolog/example3_test.pl
@@ -43,9 +43,9 @@ test(turn1_mietus_0) :-
     res0_min(turn1, mietus, state0, [state0]),
     res0_plus(turn1, mietus, state0, [state1,state2]),
     resN(turn1,mietus,state0,[state1]),
-    resAb(turn1, mietus, state0, []),
+    resAb(turn1, mietus, state0, [state0]),
     resN_trunc(turn1,mietus,state0,[state1]),
-    resAb_trunc(turn1, mietus, state0, []).
+    resAb_trunc(turn1, mietus, state0, [state0]).
 
 test(turn1_mietus_1) :-
     res0(turn1, mietus, state1, [state0, state1,state2,state3]),
@@ -79,18 +79,18 @@ test(turn2_hador_0) :-
     res0_min(turn2, hador, state0, [state0]),
     res0_plus(turn2, hador, state0, [state2,state3]),
     resN(turn2,hador,state0,[state3]),
-    resAb(turn2, hador, state0, []),
+    resAb(turn2, hador, state0, [state0]),
     resN_trunc(turn2,hador,state0,[state3]),
-    resAb_trunc(turn2, hador, state0, []).
+    resAb_trunc(turn2, hador, state0, [state0]).
 
 test(turn2_hador_1) :-
     res0(turn2, hador, state1, [state0, state1, state2, state3]),
     res0_min(turn2, hador, state1, [state1]),
     res0_plus(turn2, hador, state1, [state2,state3]),
-    resN(turn2,hador,state1,[state3]),
-    resAb(turn2, hador, state1, []),
-    resN_trunc(turn2,hador,state1,[state3]),
-    resAb_trunc(turn2, hador, state1, []).
+    resN(turn2,hador,state1,[state2]),
+    resAb(turn2, hador, state1, [state1]),
+    resN_trunc(turn2,hador,state1,[state2]),
+    resAb_trunc(turn2, hador, state1, [state1]).
 
 test(turn2_hador_2) :-
     res0(turn2, hador, state2, [state0, state1]),
