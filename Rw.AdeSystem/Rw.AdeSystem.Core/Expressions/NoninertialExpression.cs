c3affca2f477707caf207e574bec5ee1475014ed
Justyna Janczyszyn <janczyszynj@student.mini.pw.edu.pl>
2014-05-28 02:48:53 +0200

    parsowanie noninertial

--- Rw.AdeSystem/Rw.AdeSystem.Core/Expressions/NoninertialExpression.cs
+++ Rw.AdeSystem/Rw.AdeSystem.Core/Expressions/NoninertialExpression.cs
@@ -2,13 +2,17 @@ namespace Rw.AdeSystem.Core.Expressions
 {
     public class NoninertialExpression : Expression
     {
+        public string ActionName { get; set; }
+
         public NoninertialExpression(string line) : base(line)
         {
+            var tokens = line.Trim().Split(' ');
+            ActionName = tokens[1];
         }
 
         public override void ToProlog()
         {
-            throw new System.NotImplementedException();
+            AdeSystem.PrologEngine.AssertFact("sinertial("+ActionName+")");
         }
     }
 }
