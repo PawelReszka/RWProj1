8e52156b749d2c109837a10855293f6155bf3162
Justyna Janczyszyn <janczyszynj@student.mini.pw.edu.pl>
2014-05-28 02:30:23 +0200

    parsowanie preserves

--- Rw.AdeSystem/Rw.AdeSystem.Core/Expressions/PreservesExpression.cs
+++ Rw.AdeSystem/Rw.AdeSystem.Core/Expressions/PreservesExpression.cs
@@ -2,13 +2,29 @@ namespace Rw.AdeSystem.Core.Expressions
 {
     public class PreservesExpression : Expression
     {
+        public string ActionName { get; set; }
+        public string ExecutorName { get; set; }
+        public List<string> Fluents  = new List<string>();
+
         public PreservesExpression(string line) : base(line)
         {
+            //nie wiem czy dobrze rozumiem ale np to moze sparsowac : Entice by hador preserves a
+            var tokens = line.Trim().Split(' ');
+            ActionName = tokens[0];
+            ExecutorName = tokens[2]; //2 zeby ominac by? nie wiem czy jakos inaczej trzeba to zrobic
+            Fluents.Add(tokens.Last());
         }
 
         public override void ToProlog()
         {
-            throw new System.NotImplementedException();
+            var fluents = new StringBuilder();
+            for (int i = 0; i < Fluents.Count; i++)
+            {
+                fluents.Append(Fluents[i]);
+                if (i != Fluents.Count - 1)
+                    fluents.Append(", ");
+            }
+            AdeSystem.PrologEngine.AssertFact("preserve("+ActionName+","+ExecutorName+",["+effects+"])");
         }
     }
 }
