fbe32ee676edafbbfc5dab15cc8da8fd5a3cab2e
Tomasz Półgrabia <tpsa.s.a@gmail.com>
2014-05-29 23:45:17 +0200

    Zmiana - typically zrób by wszystkie, ale o najmniejszej abnormalności.

--- Rw.AdeSystem/prolog/engine.pl
+++ Rw.AdeSystem/prolog/engine.pl
@@ -825,20 +825,25 @@ typically_cont([HEAD|STATES], [ACTION|ACTIONS], [EXECUTOR|EXECUTORS], FLUENTS_TO
     (
         (
             EXECUTOR \= epsilon,
-            resN_trunc(ACTION, EXECUTOR, HEAD, STATES_ACTION)
+            POSS_EXECUTORS = [EXECUTOR]
         )
     ;
         (
             EXECUTOR == epsilon,
-            findall(X, executor(X),POSS_EXECUTORS),
-            member(POSS_EXECUTOR,POSS_EXECUTORS),
-            resN_trunc(ACTION, POSS_EXECUTOR, HEAD, STATES_ACTION)
+            findall(X, executor(X),POSS_EXECUTORS)
         )
     ),
-    length(STATES_ACTION, N),
-    N > 0,
-    typically_cont(STATES_ACTION, ACTIONS, EXECUTORS, FLUENTS_TO),
-    typically_cont(STATES, [ACTION|ACTIONS], [EXECUTOR|EXECUTORS], FLUENTS_TO),
+    (
+        foreach(member(Y,POSS_EXECUTORS),
+            (
+                res0_trunc(ACTION, Y, HEAD, STATES_ACTION),
+                length(STATES_ACTION, N),
+                N > 0,
+                typically_cont(STATES_ACTION, ACTIONS, EXECUTORS, FLUENTS_TO)
+            )),
+        typically_cont(STATES, [ACTION|ACTIONS], [EXECUTOR|EXECUTORS], FLUENTS_TO)
+    ),
     !.
+
 typically_cont([],_,_,_).
 
