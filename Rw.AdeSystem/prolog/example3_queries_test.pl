32b1c187ab30c15d85f30b6689f666f182b15751
Tomasz Półgrabia <tpsa.s.a@gmail.com>
2014-05-29 23:49:19 +0200

    Dodano testy dla example3.

--- Rw.AdeSystem/prolog/example3_queries_test.pl
+++ Rw.AdeSystem/prolog/example3_queries_test.pl
@@ -16,5 +16,19 @@ test(always_2) :-
 test(typically_2) :-
     not(typically([switch2_off,switch1_off],[turn2,turn1],[epsilon,epsilon], [switch1_on, switch2_on])).
 
+test(possibly_3) :-
+    possibly_involved(hador, [turn2,turn1], [epsilon,epsilon]).
+test(always_3) :-
+    not(always_involved(hador, [turn2,turn1], [epsilon,epsilon])).
+test(typically_3) :-
+    not(typically_involved(hador, [turn2,turn1], [epsilon,epsilon])).
+
+test(possibly_4) :-
+    possibly_involved(mietus, [turn2,turn1], [epsilon,epsilon]).
+test(always_4) :-
+    not(always_involved(mietus, [turn2,turn1], [epsilon,epsilon])).
+test(typically_4) :-
+    not(typically_involved(mietus, [turn2,turn1], [epsilon,epsilon])).
+
 
 :- end_tests(example3_queries).
