11d700b596e3ac49d7ee90789e8cde03ccd7161d
DobryMietus <araistari@op.pl>
2014-06-07 23:26:35 +0200

    initially

--- Rw.AdeSystem/Rw.AdeSystem.Core/Expressions/InitiallyExpression.cs
+++ Rw.AdeSystem/Rw.AdeSystem.Core/Expressions/InitiallyExpression.cs
@@ -18,7 +18,66 @@ namespace Rw.AdeSystem.Core.Expressions
         public override void ToProlog()
         {
             var fluents = String.Join(", ", Fluents);
-            AdeSystem.PrologEngine.AssertFact("initially(["+fluents+",not_m])");
+            AdeSystem.PrologEngine.AssertFact("initially_after([],[],["+fluents+"])");
+        }
+    }
+
+    public class AfterExpression: Expression
+    {
+        private List<string> Fluents { get; set; }
+        private List<string> Executors { get; set; }
+        private string ActionsString { get; set; }
+        private string ExecutorsString { get; set; }
+        protected string prefix;
+        public AfterExpression(string line)
+            : base(line)
+        {
+           var conditions= line.Substring(0, line.IndexOf("after")).Trim();
+            List<string> litValues;
+            List<Token> literals;
+            var expression = LogicFormulaParser.Parse(conditions, out literals, out litValues);
+            Fluents = LogicFormulaParser.GetFluentStrings(expression);
+            if(line.Contains("by"))
+            {
+                ActionsString = FluentParser.GetSubstring(line, " after ", " by ");
+                ExecutorsString = FluentParser.GetSubstring(line, " by ");  
+            }
+            else
+            {
+                ActionsString = FluentParser.GetSubstring(line, " after ");
+                char[] tab=new char[]{','};
+                var len=ActionsString.Split().Length;
+                List<string> l=new List<string>();
+                for(int i=0;i<len;i++)
+                    l.Add("epsilon");
+                ExecutorsString =  String.Join(", ", l);
+            }
+        }
+
+        public override void ToProlog()
+        {
+            var fluents = String.Join(", ", Fluents);
+            AdeSystem.PrologEngine.AssertFact(prefix + "_after([" + ActionsString + "],[" + ExecutorsString + "],[" + fluents + "])");
+        }
+
+       
+    }
+
+    public class InitiallyAfterExpression : AfterExpression
+    {
+        public InitiallyAfterExpression(string line)
+            : base(line)
+        {
+            prefix = "initially";
+        }
+    }
+    public class ObservableAfterExpression : AfterExpression
+    {
+        
+        public ObservableAfterExpression(string line)
+            : base(line.Trim().Remove(0, "observable".Length).Trim())
+        {
+            prefix = "observable";
         }
     }
 }
