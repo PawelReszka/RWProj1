64ee9c18cc464d5a5a31772ae15a4c47d154cb0a
DobryMietus <araistari@op.pl>
2014-06-07 11:31:36 +0200

    zmiana nazw kwerend z rodziny AFTER

--- Rw.AdeSystem/prolog/example2_queries_test.pl
+++ Rw.AdeSystem/prolog/example2_queries_test.pl
@@ -3,25 +3,25 @@
 :- begin_tests(example2_queries).
 
 test(possibly_1) :-
-    possibly([alive],[entice,shoot],[epsilon,hador],[not_alive,has_gun_hador]).
+    possibly_after([alive],[entice,shoot],[epsilon,hador],[not_alive,has_gun_hador]).
 test(always_1) :-
-    not(always([alive],[entice,shoot],[epsilon,hador],[not_alive,has_gun_hador])).
+    not(always_after([alive],[entice,shoot],[epsilon,hador],[not_alive,has_gun_hador])).
 test(typically_1) :-
-    not(typically([alive],[entice,shoot],[epsilon,hador],[not_alive,has_gun_hador])).
+    not(typically_after([alive],[entice,shoot],[epsilon,hador],[not_alive,has_gun_hador])).
 
 test(possibly_2) :-
-    possibly([alive],[entice,shoot],[epsilon,hador],[not_alive,not_has_gun_hador]).
+    possibly_after([alive],[entice,shoot],[epsilon,hador],[not_alive,not_has_gun_hador]).
 test(always_2) :-
-    not(always([alive],[entice,shoot],[epsilon,hador],[not_alive,not_has_gun_hador])).
+    not(always_after([alive],[entice,shoot],[epsilon,hador],[not_alive,not_has_gun_hador])).
 test(typically_2) :-
-    typically([alive],[entice,shoot],[epsilon,hador],[not_alive,not_has_gun_hador]).
+    typically_after([alive],[entice,shoot],[epsilon,hador],[not_alive,not_has_gun_hador]).
 
 test(possibly_3) :-
-    possibly([alive],[entice,shoot],[epsilon,hador],[not_alive]).
+    possibly_after([alive],[entice,shoot],[epsilon,hador],[not_alive]).
 test(always_3) :-
-    not(always([alive],[entice,shoot],[epsilon,hador],[not_alive])).
+    not(always_after([alive],[entice,shoot],[epsilon,hador],[not_alive])).
 test(typically_3) :-
-    not(typically([alive],[entice,shoot],[epsilon,hador],[not_alive])).
+    not(typically_after([alive],[entice,shoot],[epsilon,hador],[not_alive])).
 
 
 test(possibly_3x) :-
