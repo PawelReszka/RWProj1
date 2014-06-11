35670f6efa41542d032aa2d3cd1f5a3ee8d63959
Tomasz Półgrabia <tpsa.s.a@gmail.com>
2014-06-11 22:16:29 +0200

    Poprawka testów nr 4.

--- Rw.AdeSystem/prolog/example4_test.pl
+++ Rw.AdeSystem/prolog/example4_test.pl
@@ -3,9 +3,13 @@
 :- begin_tests(resy_example4).
 
 test(chown_mietus_0) :-
-    state(state0, STATE0),
-    state(state2, STATE2),
-    state(state4, STATE4),
+    state(state0, S0),
+    state(state2, S2),
+    state(state4, S4),
+
+    sort(S0,STATE0),
+    sort(S2,STATE2),
+    sort(S4,STATE4),
 
     sort([STATE0, STATE2,STATE4],L1),
     res0(chown, mietus, STATE0, L1),
@@ -17,10 +21,16 @@ test(chown_mietus_0) :-
     resAb_trunc(chown, mietus, STATE0, []).
 
 test(chown_hador_0) :-
-    state(state0, STATE0),
-    state(state1, STATE1),
-    state(state3, STATE3),
-    state(state5, STATE5),
+    state(state0, S0),
+    state(state1, S1),
+    state(state3, S3),
+    state(state5, S5),
+
+    sort(S0,STATE0),
+    sort(S1,STATE1),
+    sort(S3,STATE3),
+    sort(S5,STATE5),
+
 
     sort([STATE1, STATE3,STATE5],L1),
     res0(chown, hador, STATE0, L1),
@@ -32,12 +42,20 @@ test(chown_hador_0) :-
     resAb_trunc(chown, hador, STATE0, []).
 
 test(shoot_mietus_0) :-
-    state(state0, STATE0),
-    state(state1, STATE1),
-    state(state3, STATE3),
-    state(state5, STATE5),
-    state(state2, STATE2),
-    state(state4, STATE4),
+    state(state0, S0),
+    state(state1, S1),
+    state(state3, S3),
+    state(state5, S5),
+    state(state2, S2),
+    state(state4, S4),
+
+    sort(S0,STATE0),
+    sort(S1,STATE1),
+    sort(S2,STATE2),
+    sort(S3,STATE3),
+    sort(S4,STATE4),
+    sort(S5,STATE5),
+
 
     sort([STATE0, STATE1,STATE2,STATE3,STATE4,STATE5],L1),
     res0(shoot, mietus, STATE0, L1),
@@ -49,12 +67,20 @@ test(shoot_mietus_0) :-
     resAb_trunc(shoot, mietus, STATE0, []).
 
 test(shoot_hador_0) :-
-    state(state0, STATE0),
-    state(state1, STATE1),
-    state(state3, STATE3),
-    state(state5, STATE5),
-    state(state2, STATE2),
-    state(state4, STATE4),
+    state(state0, S0),
+    state(state1, S1),
+    state(state3, S3),
+    state(state5, S5),
+    state(state2, S2),
+    state(state4, S4),
+
+    sort(S0,STATE0),
+    sort(S1,STATE1),
+    sort(S2,STATE2),
+    sort(S3,STATE3),
+    sort(S4,STATE4),
+    sort(S5,STATE5),
+
 
     sort([STATE0, STATE1,STATE2,STATE3,STATE4,STATE5],L1),
     sort([STATE0,STATE2],L2),
@@ -68,9 +94,14 @@ test(shoot_hador_0) :-
     resAb_trunc(shoot, hador, STATE0, L2).
 
 test(shoot_mietus_1) :-
-    state(state1, STATE1),
-    state(state5, STATE5),
-    state(state4, STATE4),
+    state(state1, S1),
+    state(state5, S5),
+    state(state4, S4),
+
+    sort(S1,STATE1),
+    sort(S4,STATE4),
+    sort(S5,STATE5),
+
 
     res0(shoot, mietus, STATE1, [STATE4,STATE5]),
     res0_min(shoot,mietus, STATE1, [STATE5]),
@@ -81,12 +112,20 @@ test(shoot_mietus_1) :-
     resAb_trunc(shoot, mietus, STATE1, []).
 
 test(shoot_hador_1) :-
-    state(state0, STATE0),
-    state(state1, STATE1),
-    state(state3, STATE3),
-    state(state5, STATE5),
-    state(state2, STATE2),
-    state(state4, STATE4),
+    state(state0, S0),
+    state(state1, S1),
+    state(state3, S3),
+    state(state5, S5),
+    state(state2, S2),
+    state(state4, S4),
+
+    sort(S0,STATE0),
+    sort(S1,STATE1),
+    sort(S2,STATE2),
+    sort(S3,STATE3),
+    sort(S4,STATE4),
+    sort(S5,STATE5),
+
 
     sort([STATE0, STATE1,STATE2,STATE3,STATE4,STATE5],L1),
     res0(shoot, hador, STATE1, L1),
@@ -98,12 +137,20 @@ test(shoot_hador_1) :-
     resAb_trunc(shoot, hador, STATE1, []).
 
 test(shoot_mietus_2) :-
-    state(state0, STATE0),
-    state(state1, STATE1),
-    state(state3, STATE3),
-    state(state5, STATE5),
-    state(state2, STATE2),
-    state(state4, STATE4),
+    state(state0, S0),
+    state(state1, S1),
+    state(state3, S3),
+    state(state5, S5),
+    state(state2, S2),
+    state(state4, S4),
+
+    sort(S0,STATE0),
+    sort(S1,STATE1),
+    sort(S2,STATE2),
+    sort(S3,STATE3),
+    sort(S4,STATE4),
+    sort(S5,STATE5),
+
 
     sort([STATE0, STATE1,STATE2,STATE3,STATE4,STATE5],L1),
     res0(shoot, mietus, STATE2, L1),
@@ -115,12 +162,20 @@ test(shoot_mietus_2) :-
     resAb_trunc(shoot, mietus, STATE2, []).
 
 test(shoot_hador_2) :-
-    state(state0, STATE0),
-    state(state1, STATE1),
-    state(state3, STATE3),
-    state(state5, STATE5),
-    state(state2, STATE2),
-    state(state4, STATE4),
+    state(state0, S0),
+    state(state1, S1),
+    state(state3, S3),
+    state(state5, S5),
+    state(state2, S2),
+    state(state4, S4),
+
+    sort(S0,STATE0),
+    sort(S1,STATE1),
+    sort(S2,STATE2),
+    sort(S3,STATE3),
+    sort(S4,STATE4),
+    sort(S5,STATE5),
+
 
     sort([STATE0, STATE1,STATE2,STATE3,STATE4,STATE5],L1),
     res0(shoot, hador, STATE2, L1),
@@ -134,9 +189,14 @@ test(shoot_hador_2) :-
     resAb_trunc(shoot, hador, STATE2, L2).
 
 test(shoot_mietus_3) :-
-    state(state3, STATE3),
-    state(state5, STATE5),
-    state(state4, STATE4),
+    state(state3, S3),
+    state(state5, S5),
+    state(state4, S4),
+
+    sort(S3,STATE3),
+    sort(S4,STATE4),
+    sort(S5,STATE5),
+
 
     res0(shoot, mietus, STATE3, [STATE4,STATE5]),
     res0_min(shoot,mietus, STATE3, [STATE5]),
@@ -147,12 +207,20 @@ test(shoot_mietus_3) :-
     resAb_trunc(shoot, mietus, STATE3, []).
 
 test(shoot_hador_3) :-
-    state(state0, STATE0),
-    state(state1, STATE1),
-    state(state3, STATE3),
-    state(state5, STATE5),
-    state(state2, STATE2),
-    state(state4, STATE4),
+    state(state0, S0),
+    state(state1, S1),
+    state(state3, S3),
+    state(state5, S5),
+    state(state2, S2),
+    state(state4, S4),
+
+    sort(S0,STATE0),
+    sort(S1,STATE1),
+    sort(S2,STATE2),
+    sort(S3,STATE3),
+    sort(S4,STATE4),
+    sort(S5,STATE5),
+
 
     sort([STATE0, STATE1,STATE2,STATE3,STATE4,STATE5],L1),
     res0(shoot, hador, STATE3, L1),
@@ -164,12 +232,20 @@ test(shoot_hador_3) :-
     resAb_trunc(shoot, hador, STATE3, []).
 
 test(shoot_mietus_4) :-
-    state(state0, STATE0),
-    state(state1, STATE1),
-    state(state3, STATE3),
-    state(state5, STATE5),
-    state(state2, STATE2),
-    state(state4, STATE4),
+    state(state0, S0),
+    state(state1, S1),
+    state(state3, S3),
+    state(state5, S5),
+    state(state2, S2),
+    state(state4, S4),
+
+    sort(S0,STATE0),
+    sort(S1,STATE1),
+    sort(S2,STATE2),
+    sort(S3,STATE3),
+    sort(S4,STATE4),
+    sort(S5,STATE5),
+
 
 
     sort([STATE0, STATE1,STATE2,STATE3,STATE4,STATE5],L1),
@@ -182,13 +258,19 @@ test(shoot_mietus_4) :-
     resAb_trunc(shoot, mietus, STATE4, []).
 
 test(shoot_hador_4) :-
-    state(state0, STATE0),
-    state(state1, STATE1),
-    state(state3, STATE3),
-    state(state5, STATE5),
-    state(state2, STATE2),
-    state(state4, STATE4),
+    state(state0, S0),
+    state(state1, S1),
+    state(state3, S3),
+    state(state5, S5),
+    state(state2, S2),
+    state(state4, S4),
 
+    sort(S0,STATE0),
+    sort(S1,STATE1),
+    sort(S2,STATE2),
+    sort(S3,STATE3),
+    sort(S4,STATE4),
+    sort(S5,STATE5),
 
     sort([STATE0,STATE1,STATE2,STATE3,STATE4,STATE5],L1),
     res0(shoot, hador, STATE4, L1),
@@ -202,8 +284,12 @@ test(shoot_hador_4) :-
     resAb_trunc(shoot, hador, STATE4, [STATE0]).
 
 test(shoot_mietus_5) :-
-    state(state5, STATE5),
-    state(state4, STATE4),
+    state(state5, S5),
+    state(state4, S4),
+
+    sort(S4,STATE4),
+    sort(S5,STATE5),
+
 
     res0(shoot, mietus, STATE5, [STATE4,STATE5]),
     res0_min(shoot,mietus, STATE5, [STATE5]),
@@ -214,12 +300,20 @@ test(shoot_mietus_5) :-
     resAb_trunc(shoot, mietus, STATE5, []).
 
 test(shoot_hador_5) :-
-    state(state0, STATE0),
-    state(state1, STATE1),
-    state(state3, STATE3),
-    state(state5, STATE5),
-    state(state2, STATE2),
-    state(state4, STATE4),
+    state(state0, S0),
+    state(state1, S1),
+    state(state3, S3),
+    state(state5, S5),
+    state(state2, S2),
+    state(state4, S4),
+
+    sort(S0,STATE0),
+    sort(S1,STATE1),
+    sort(S2,STATE2),
+    sort(S3,STATE3),
+    sort(S4,STATE4),
+    sort(S5,STATE5),
+
 
     sort([STATE0,STATE1,STATE2,STATE3,STATE4,STATE5],L1),
 
