2903e166b9a054c044bf1f9c1888865a87e0779d
DobryMietus <araistari@op.pl>
2014-06-11 22:29:43 +0200

    poprawione testy

--- Rw.AdeSystem/prolog/example3_queries_test.pl
+++ Rw.AdeSystem/prolog/example3_queries_test.pl
@@ -3,18 +3,18 @@
 :- begin_tests(example3_queries).
 
 test(possibly_1) :-
-    possibly_after([switch2_off,switch1_off],[turn1,turn2],[epsilon,epsilon], [switch1_on, switch2_on]).
+    possibly_after([[switch2_off,switch1_off]],[turn1,turn2],[epsilon,epsilon], [[switch1_on, switch2_on]]).
 test(always_1) :-
-    not(always_after([switch2_off,switch1_off],[turn1,turn2],[epsilon,epsilon], [switch1_on, switch2_on])).
+    not(always_after([[switch2_off,switch1_off]],[turn1,turn2],[epsilon,epsilon], [[switch1_on, switch2_on]])).
 test(typically_1) :-
-    typically_after([switch2_off,switch1_off],[turn1,turn2],[epsilon,epsilon], [switch1_on, switch2_on]).
+    typically_after([[switch2_off,switch1_off]],[turn1,turn2],[epsilon,epsilon], [[switch1_on, switch2_on]]).
 
 test(possibly_2) :-
-    possibly_after([switch2_off,switch1_off],[turn2,turn1],[epsilon,epsilon], [switch1_on, switch2_on]).
+    possibly_after([[switch2_off,switch1_off]],[turn2,turn1],[epsilon,epsilon], [[switch1_on, switch2_on]]).
 test(always_2) :-
-    not(always_after([switch2_off,switch1_off],[turn2,turn1],[epsilon,epsilon], [switch1_on, switch2_on])).
+    not(always_after([[switch2_off,switch1_off]],[turn2,turn1],[epsilon,epsilon], [[switch1_on, switch2_on]])).
 test(typically_2) :-
-    typically_after([switch2_off,switch1_off],[turn2,turn1],[epsilon,epsilon], [switch1_on, switch2_on]).
+    typically_after([[switch2_off,switch1_off]],[turn2,turn1],[epsilon,epsilon], [[switch1_on, switch2_on]]).
 % powinno byc tu TRUE, poniewaz turn1 w stanie 3 przez hador prowadzi nie do 3 tylko 2 -N
 
 test(possibly_3) :-
